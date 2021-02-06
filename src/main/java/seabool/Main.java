dbe7ede35b2203d97c8c8a0a619d82e9fc9f6895
Sebastian Stachecki <xenophobiouspl@gmail.com>
2021-02-06 18:59:05 +0100

    init: add dependency lang3

--- src/main/java/seabool/Main.java
+++ src/main/java/seabool/Main.java
@@ -1,9 +1,19 @@
 package seabool;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Main {
 
-  public static void main(String[] args){
-    System.out.print("Hello World");
-  }
+	public int countWords(String words){
+		String[] separateWords = StringUtils.split(words, ' ');
+		return (separateWords == null) ? 0 : separateWords.length;
+	}
+
+	public static void main(String[] args){
+		Main main = new Main();
+		String text = "Hey hello hi yo";
+		System.out.print(text);
+		System.out.print("Number of words: " + Integer.toString(main.countWords(text)));
+	}
 
 }
